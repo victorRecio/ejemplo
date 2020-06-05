46854d864fd4d3c29d2d34da5a76518e65b16c48
Victor Recio <vrecio.intecsol@gmail.com>
2020-06-05 11:17:36 -0400

    Modulos

---
+++ app.py
@@ -0,0 +1,42 @@
+from flask import Flask, render_template, request, redirect, url_for, flash 
+app = Flask(__name__)
+
+import pymysql.cursors
+#Modulos
+import ventas.articulos as Articulos
+
+#articulos.add_articulo();
+
+db = pymysql.connect(
+    host="localhost", port=3306, user="root",
+    passwd="1234", db="almacen"
+    #esto es prueba.
+)
+cur = db.cursor()
+
+#settings
+app.secret_key = 'mysecretkey'
+
+
+@app.route('/', methods=['GET', 'POST'])
+def index():
+    return render_template('index.html')
+
+"""A qui Todo con Respecto a la Creacion y manipulacion de articulos"""
+try:
+    app.route('/add_articulo', methods=['POST'])
+    Articulos.add_articulo()
+    @app.route('/edit_articulo/<id>',methods=['POST'])
+    Articulos.edit_articulo()
+    @app.route('/up_articulo/<id>',methods=['POST'])
+    Articulos.up_articulo()
+    @app.route('/delete_articulo/<string:id>',methods=['POST'])
+    Articulos.delete_articulo()
+    @app.route('/find_articulo',methods=['POST'])
+    Articulos.find_articulo()
+except Exception as err:
+    print(err)
+    print("Ooosp! Ocurrio un error")
+
+if __name__ == '__main__':
+    app.run(port = 3000, debug = True)    
--- ventas/articulos.py
+++ ventas/articulos.py
@@ -4,11 +4,12 @@ app = Flask(__name__)
 #from flask_mysqldb import MySQL
 import pymysql.cursors
 
-conn = pymysql.connect(
+db = pymysql.connect(
     host="localhost", port=3306, user="root",
     passwd="1234", db="almacen"
+    #esto es prueba.
 )
-cur = conn.cursor()
+cur = db.cursor()
 """
 # Mysql Connection
 app.config['MYSQL_HOST'] = 'localhost'
@@ -21,35 +22,26 @@ mysql = MySQL(app)
 #settings
 app.secret_key = 'mysecretkey'
 
-
-@app.route('/')
-def hello_world():
-    return 'Hola amigos de Geeky Theory!'
-    
-"""
-@app.route('/', methods=['GET', 'POST'])
 def index():
-    #cur = mysql.connection.cursor()
-    cur.execute('SELECT * FROM tbl_articulos')
-    data = cur.fetchall()
-    return render_template("articulos.html", art = data)
-"""
-app.route('/add_articulo',methods=['POST'])
+    return render_template('index.html')
+
 def add_articulo():
-    if request.method == "POST":
+    if request.method == 'POST':
+        print("hola")
+        """
         details = request.form
         firstName = details['nomb_art']
         price_art = details['price_art']
-        description = details['desc_art']
-
-        #cur = mysql.connection.cursor()
-        cur.execute("INSERT INTO tbl_articulos(nombre, precio, description) VALUES (%s, %s)", (firstName, price_art, desc_art))
-        mysql.connection.commit()
-        cur.close()
-        flash('Articulo added Sucessfully')
+        description = details['desc_art']"""
+        #details = request.form
+        firstName  = request.form['nomb_art']
+        price_art  = request.form['price_art']
+        description = request.form['desc_art']
+        print(firstName)
+        print(price_art)
+        print(description)
         return redirect(url_for('index'))
-
-@app.route('/edit_articulo/<id>',methods=['POST'])
+        
 def edit_articulo(id):
     #cur = mysql.connection.cursor()
     cur.execute("SELECT * FROM tbl_articulos where id = %s",(id))
@@ -58,7 +50,7 @@ def edit_articulo(id):
     flash('Articulo select Sucessfully')
     return redirect(url_for('edit_articulo.html',art = data))
 
-@app.route('/up_articulo/<id>',methods=['POST'])
+
 def up_articulo():
     #cur = mysql.connection.cursor()
     if request.method == "POST":
@@ -72,32 +64,28 @@ def up_articulo():
                         precio = %s,
                         description = %s 
                     where id = %s
-                    """,(firstName, price_art, desc_art,id)
+                    """,(firstName, price_art, description,id)
         )
-        mysql.connection.commit()
+        cur.connection.commit()
         cur.close()
         flash('Articulo Update Sucessfully')
         return redirect(url_for('index'))
 
-@app.route('/delete_articulo/<string:id>',methods=['POST'])
+
 def delete_articulo(id):
     #cur = mysql.connection.cursor()
     cur.execute("DELETE FROM tbl_articulos where id = {0}",format(id))
-    mysql.connection.commit()
+    cur.connection.commit()
     cur.close()
     flash('Articulo Remove Sucessfully')
     return redirect(url_for('index'))
 
-@app.route('/find_articulo',methods=['POST'])
+
 def find_articulo():
     #cur = mysql.connection.cursor()
     cur.execute("SELECT * FROM tbl_articulos where id = {0}",format(id))
     data = cur.fetchall()
     cur.close()
     flash('Articulo Find Sucessfully')
-    return ""
-    #return render_template('templates/articulos.html', art = data)
-    
-
-if __name__ == '__main__':
-    app.run(port = 3000, debug = True)
+    #return ""
+    return render_template('templates/articulos.html', art = data)
